0630faf868bc77ca2876efa5c1259b64551e7dec
ohkubo kohei <kuboon@trick-with.net>
2020-10-17 06:40:36 +0900

    debug.c
    debug.c

--- goppa/op.c
+++ goppa/op.c
@@ -1,29 +1,6 @@
 #ifndef DEG
 #include "struct.h"
 #endif
-#include <stdbool.h>
-#include <execinfo.h>
-
-/* Obtain a backtrace and print it to stdout. */
-void
-print_trace (void)
-{
-  void *array[10];
-  char **strings;
-  int size, i;
-
-  size = backtrace (array, 10);
-  strings = backtrace_symbols (array, size);
-  if (strings != NULL)
-    {
-
-      printf ("Obtained %d stack frames.\n", size);
-      for (i = 0; i < size; i++)
-        printf ("%s\n", strings[i]);
-    }
-
-  free (strings);
-}
 
 //有限体の元の逆数
 unsigned short
